0af3adc96238cf4409d8cf68e391d83d5c90ed1b
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-12-18 14:27:47 +0100

    setup tailwind for project

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.scss'
 import { BrowserRouter, Route, Routes } from "react-router";
+import EnterNamePage from './pages/EnterNamePage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<EnterNamePage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
