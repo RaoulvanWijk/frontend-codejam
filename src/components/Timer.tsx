cf6b0bb75b3ee976a331027b3b92428f2676cf35
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2025-01-15 11:20:28 +0100

    added a timer that keeps track of the time wasted

---
+++ src/components/Timer.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+
+export default function Timer() {
+    const [timer, setTimer] = React.useState(0)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer((timer) => timer + 1)
+        }, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
+  return (
+    <p className='fixed bottom-6 left-6 text-4xl font-normal'>
+      Time: {
+        `${Math.floor(timer / 60)}:${timer % 60 < 10 ? `0${timer % 60}` : timer % 60}`
+      }
+    </p>
+  )
+}
--- src/pages/EnterNamePage.tsx
+++ src/pages/EnterNamePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import EnterNameForm from '../components/EnterNameForm'
+import Timer from '../components/Timer'
 
 export default function EnterNamePage() {
   return (
     <div className='bg-bg h-screen overflow-x-hidden'>
         <Header />
         <EnterNameForm />
+        <Timer />
     </div>
   )
 }
