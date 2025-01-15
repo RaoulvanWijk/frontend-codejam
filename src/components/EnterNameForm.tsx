554714c0dd5de238332c7b7a6d7b1f07ae803dd3
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2025-01-15 11:10:31 +0100

    made the layout for the input form

--- src/components/EnterNameForm.tsx
+++ src/components/EnterNameForm.tsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 export default function EnterNameForm() {
   return (
-    <div>
-    Enter name
+    <div className='flex flex-col items-center space-y-12 pt-12'>
+      <p className='font-irishGrover italic text-white text-8xl font-normal border-[#3700FF] border-8 bg-[#D9D9D9] rounded-lg w-[45rem] h-20 leading-10'>Your name...</p>
+
+      <div className='grid grid-cols-10 gap-6 gap-y-12'>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1 col-start-2'>Z</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>M</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>F</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>W</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>N</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>Y</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>A</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>Q</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1 col-start-1'>B</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>P</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>S</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>U</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>G</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>J</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>K</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>L</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>X</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>T</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1 col-start-2'>R</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>I</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>C</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>O</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>E</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>H</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>←</p>
+        <p className='flex justify-center items-center content-center text-5xl h-16 w-16 rounded-full bg-red-600 border-2 border-black grid-col-1'>V</p>
+      </div>
+
     </div>
   )
 }
